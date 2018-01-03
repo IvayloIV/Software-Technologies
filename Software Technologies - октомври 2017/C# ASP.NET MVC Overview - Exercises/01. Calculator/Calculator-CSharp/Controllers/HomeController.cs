5455014fbbfdcaf9426bfde771f691d6fca6a869
Your Name <ivvo98@abv.bg>
2018-01-03 11:39:25 +0200

    C#CalculatorExer

---
+++ Software Technologies - октомври 2017/C# ASP.NET MVC Overview - Exercises/01. Calculator/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Calculator_CSharp.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            decimal result = 0;
+
+            if (calculator.Operator == "+")
+            {
+                result = calculator.LeftOperand + calculator.RightOperand;
+            }
+            else if (calculator.Operator == "-")
+            {
+                result = calculator.LeftOperand - calculator.RightOperand;
+            }
+            else if (calculator.Operator == "*")
+            {
+                result = calculator.LeftOperand * calculator.RightOperand;
+            }
+            else if (calculator.Operator == "/")
+            {
+                result = calculator.LeftOperand / calculator.RightOperand;
+            }
+            else if (calculator.Operator == "^")
+            {
+                if (calculator.RightOperand == 0)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = 1;
+                    for (int i = 1; i <= calculator.RightOperand; i++)
+                    {
+                        result *= calculator.LeftOperand;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}

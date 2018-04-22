f391d9043da7ad092a63c6ace9877aa7b5582f58
IvayloIV <ivvo98@abv.bg>
2018-04-22 22:35:03 +0300

    My exam

---
+++ Software Technologies - Exams/Cat_Shop_CUD/csharp/CatShop/Models/Cat.cs
@@ -0,0 +1,21 @@
+﻿namespace CatShop.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Cat
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(65535)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(65535)]
+        public string Nickname { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
